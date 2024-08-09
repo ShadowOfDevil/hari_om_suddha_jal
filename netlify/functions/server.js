9c79c8778b79f071987d37236fb3f6cfe1e70345
ajay-prajapati <ajayprajapati20031999@gmail.com>
2024-08-09 20:11:52 +0530

    initial commit

---
+++ netlify/functions/server.js
@@ -0,0 +1,12 @@
+import jsonServer from "json-server";
+
+const server = jsonServer.create();
+const router = jsonServer.router('../../db.json'); // Path to your JSON file
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(8888, () => {
+  console.log('JSON Server is running');
+});
